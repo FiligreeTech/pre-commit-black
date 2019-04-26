8ba8298f07ec5bfb868bdb8de40eb369d6896e1d
Nicholas Hutchinson <nick.hutchinson@filigree.tech>
2019-04-26 12:33:23 +0100

    Black 19.3b0

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 
 setup(
-    name="pre_commit_dummy_package", version="0.0.0", install_requires=["black==18.9b0"]
+    name="pre_commit_dummy_package", version="0.0.0", install_requires=["black==19.3b0"]
 )
