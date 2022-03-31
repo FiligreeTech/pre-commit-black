09b9044f2e1d1a9e5f40c8e11078833f1ed1dde9
Tom Walker <tom@filigree.tech>
2022-03-31 10:25:15 +0100

    Bump setup.py

--- setup.py
+++ setup.py
@@ -2,5 +2,5 @@ from setuptools import setup
 
 
 setup(
-    name="pre_commit_dummy_package", version="0.0.0", install_requires=["black==19.3b0"]
+    name="pre_commit_dummy_package", version="0.0.0", install_requires=["click==8.0.0", "black==19.3b0"]
 )
